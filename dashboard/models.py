d16d9f47012e84419926962424e55c3eb0359156
Pushpa Harshitha Sangu <pharshitha2005@gmail.com>
2025-06-21 14:28:46 +0530

    Adding files

---
+++ dashboard/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Hospital(models.Model):
+  hospital_name = models.CharField(max_length=100, unique=True)
+  hospital_id = models.CharField(max_length=100, primary_key=True)
+  ip_addr = models.CharField(max_length=30)
+  password = models.CharField(max_length=256)  # Make sure to hash the password
+
+  def __str__(self):
+        return self.hospital_id
---
+++ dashboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'dashboard'
+
+urlpatterns = [
+  path("",views.homepage,name='homepage'),
+  path("pushpa/",views.display,name='display'),
+  path("hospital_login/",views.h_login,name="h_login"),
+  path('hospital_register/',views.h_register,name="h_register"),
+]
